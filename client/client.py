abccdb0fc55349dc7ddfcf53d968151b98cd32dc
Lorenzo de Souza Oliveira <lorenzo.oliveira@discente.univasf.edu.br>
2026-09-07 22:19:05 -0300

    Mensagem de erro caso o servidor não esteja rodando

--- client/client.py
+++ client/client.py
@@ -5,14 +5,13 @@ import tasks_pb2
 import tasks_pb2_grpc
 
 def run():
-    response = []
-
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = tasks_pb2_grpc.TasksStub(channel)
-
-        response = stub.getTasks(tasks_pb2.getTasksRequest())
-
-    print(response.taks)
+    try:
+        with grpc.insecure_channel('localhost:50051') as channel:
+            stub = tasks_pb2_grpc.TasksStub(channel)
+            response = stub.GetTasks(tasks_pb2.GetTasksRequest())
+        print(response.tasks)
+    except grpc._channel._InactiveRpcError:
+        print("Erro: Servidor não está rodando. Inicie o servidor primeiro.")
 
 
 if __name__ == '__main__':
